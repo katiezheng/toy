dd4a0cb768b747f9a8bf6dfc3764b527094e48f9
katiezheng <k.zheng64@gmail.com>
2014-04-03 19:29:43 -0400

    toy with 2 scenes

---
+++ Assets/cubemove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubemove: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+		void FixedUpdate () {
+			
+			if ( Input.GetKey (KeyCode.UpArrow) )
+			{
+				rigidbody.AddForce ( transform.up * 20f );
+			}
+
+		if ( Input.GetKey (KeyCode.DownArrow) )
+		{
+			rigidbody.AddForce ( -transform.up * 20f );
+		}
+
+		if ( Input.GetKey (KeyCode.RightArrow) )
+		{
+			rigidbody.AddForce ( transform.right * 20f );
+		}
+			
+			if ( Input.GetKey (KeyCode.LeftArrow) )
+			{
+				rigidbody.AddForce ( -transform.right * 20f );
+			}
+			// rigidbody.AddForce( new Vector3(0f, 10f, 0f) );
+		}
+		
+}
---
+++ Assets/switchscene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchscene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// change the scene after 60 seconds
+		if ( Time.timeSinceLevelLoad > 15f ) {
+			AutoFade.LoadLevel("Second" ,1,1,Color.black);
+		}
+		
+		// change the scene if you press R
+		if (Input.GetKey (KeyCode.R) ) 
+		{
+			AutoFade.LoadLevel("Second" ,1,1,Color.black);
+		}
+
+	}
+}
